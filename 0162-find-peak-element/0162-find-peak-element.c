fc2442725ddd58787b7e49a04ea7440b26f5fb41
Yash Mohadikar <156001644+Yashx073@users.noreply.github.com>
2026-07-16 21:57:37 +0530

    Time: 0 ms (100%), Space: 8.9 MB (28.98%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.c
@@ -0,0 +1,9 @@
+int findPeakElement(int* nums, int numsSize) {
+    int count = 0;
+    for(int i = 1; i < numsSize; i++){
+        if(nums[i-1] <  nums[i]){
+            count = i;
+        }
+    }
+    return count;
+}

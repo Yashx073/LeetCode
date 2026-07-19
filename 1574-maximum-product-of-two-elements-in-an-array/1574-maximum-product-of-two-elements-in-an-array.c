da363225caac042b7b3c5557f7b05051211ace97
Yash Mohadikar <156001644+Yashx073@users.noreply.github.com>
2026-07-19 11:07:00 +0530

    Time: 3 ms (44.44%), Space: 9.1 MB (55.56%) - LeetHub

---
+++ 1574-maximum-product-of-two-elements-in-an-array/1574-maximum-product-of-two-elements-in-an-array.c
@@ -0,0 +1,8 @@
+int compare(const void* a, const void* b){
+    return (*(int *)b - *(int *)a);
+}
+
+int maxProduct(int* nums, int numsSize) {
+    qsort(nums, numsSize, sizeof(int), compare);
+    return (nums[0]-1)*(nums[1]-1);
+}

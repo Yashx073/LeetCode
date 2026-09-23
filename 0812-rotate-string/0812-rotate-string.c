0adbbfc083ec8390d85c60c42ce8f2de860d2a47
Yash Mohadikar <156001644+Yashx073@users.noreply.github.com>
2026-09-23 22:46:59 +0530

    Time: 0 ms (100%), Space: 8.4 MB (79.05%) - LeetHub

---
+++ 0812-rotate-string/0812-rotate-string.c
@@ -0,0 +1,16 @@
+bool rotateString(char* s, char* goal) {
+
+    if(strlen(s) != strlen(goal))
+        return false;
+
+    char* temp = malloc(2 * strlen(s) + 1);
+
+    strcpy(temp, s);
+    strcat(temp, s);
+
+    bool ans = strstr(temp, goal) != NULL;
+
+    free(temp);
+
+    return ans;
+}
